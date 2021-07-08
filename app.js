a04a464557c5c9eca0978deaa2f00d28c18f8d3d
BaRyan1 <76247481+BaRyan1@users.noreply.github.com>
2021-07-08 18:55:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,41 +2,41 @@ let buttonEnter = document.getElementById('enter');
 let userInput = document.getElementById('userInput');
 let ul = document.querySelector('ul');
 
-function inputIsNotEmpty() {
+function inputLength() {
     return userInput.value.length > 0;
 }
 
 function createTodo() {
-    let li = document.createElement('li');
+    let li = document.createElement("li");
     li.appendChild(document.createTextNode(userInput.value));
     ul.appendChild(li);
     userInput.value = '';
 
-    li.addEventListener('click', function() {
+
+    li.onclick = function() {
         li.classList.toggle('done');
-    })
+    }
 
     let deleteButton = document.createElement('button');
-    deleteButton.appendChild(document.createTextNode('x'));
+    deleteButton.appendChild(document.createTextNode('X'));
     li.appendChild(deleteButton);
-    deleteButton.addEventListener('click', deleteTodoItem)
-    
+    deleteButton.addEventListener('click', deleteTodoItem);
+
     function deleteTodoItem() {
         li.classList.add('delete');
     }
 }
 
-function changeListAfterKeyPress(event) {
-    if (inputIsNotEmpty() && event.which == 13) {
+function changeListAfterKeypress(event) {
+    if (inputLength() && event.which === 13) {
         createTodo();
     }
 }
 
-function changeListAfterButtonPress(event) {
-    if (inputIsNotEmpty()) {
+userInput.addEventListener('keypress', changeListAfterKeypress);
+
+buttonEnter.onclick = function() { 
+    if (inputLength()) {
         createTodo();
     }
-}
-
-userInput.addEventListener('keypress', changeListAfterKeyPress);
-buttonEnter.addEventListener('click', changeListAfterButtonPress);
+};
